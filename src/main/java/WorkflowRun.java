497cf7a12b1af4959e2155c9e46e080fa2250edb
silver__toad <den.shaikhatarov@gmail.com>
2025-12-25 18:13:03 +0100

    feat: add more appropriate fields to WorkflowRun model

--- src/main/java/WorkflowRun.java
+++ src/main/java/WorkflowRun.java
@@ -3,16 +3,33 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class WorkflowRun {
+    /// Timestamp of when this workflow run was queued
+    @JsonProperty("created_at")
+    private String createdAt;
+    /// ID of this particular workflow run
     @JsonProperty("id")
-    private long runId;              // ID of this particular workflow run
+    private long runId;
+    /// ID of the workflow, of which this is an instance
     @JsonProperty("workflow_id")
-    private long workflowId;         // ID of the workflow, of which this is an instance
+    private long workflowId;
+    /// Name of this workflow run
+    @JsonProperty("name")
     private String name;
+    /// Event, which triggered the creation of this workflow run
+    @JsonProperty("event")
+    private String event;
+    /// Current status of this workflow run
+    @JsonProperty("status")
+    private String status;
+    /// Conclusion, with which this workflow run concluded
+    @JsonProperty("conclusion")
+    private String conclusion;
+    /// Branch, under which this workflow run was created
     @JsonProperty("head_branch")
-    private String headBranch;      //
-    private String event;           //
-    private String status;          //
-    private String conclusion;      //
+    private String headBranch;
+    /// SHA of the last commit in the head branch
+    @JsonProperty("head_sha")
+    private String headSha;
 
     public String toString(){
         String output = "";
